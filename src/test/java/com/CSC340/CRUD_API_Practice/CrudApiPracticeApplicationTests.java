d392ff2224335d7f86eec619edf770fdc9a55797
Alexander Hubbard <alexanderhubbard05@gmail.com>
2026-03-04 11:24:12 -0500

    Initial file commit

---
+++ src/test/java/com/CSC340/CRUD_API_Practice/CrudApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC340.CRUD_API_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
